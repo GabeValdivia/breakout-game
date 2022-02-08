766777266b62797f0ed9e2986134383cf9834bc2
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-07 19:52:10 -0600

    Write event handlers for show rules panel and hide rules button

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
